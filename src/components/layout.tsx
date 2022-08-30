1a369b6a7bf674ccaed03da674d4514d5d4ad969
Fabio Gianini <faebeee@gmail.com>
2022-08-30 21:07:01 +0200

    Improve UI

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,57 +1,62 @@
-import { AppBar, Box, Button, Drawer, Toolbar, Typography } from "@mui/material";
-import { Settings } from "./settings";
-import { PropsWithChildren, useMemo } from "react";
+import {AppBar, Box, Button, Drawer, Toolbar, Typography} from "@mui/material";
+import {Settings} from "./settings";
+import {PropsWithChildren, useMemo} from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import {useRouter} from "next/router";
 import qs from 'qs';
+import {version, name} from "../../package.json"
 
 export type LayoutProps = PropsWithChildren<{
     sub?: string;
+    userName?: string;
 }>;
 const drawerWidth = 340;
 
-export const Layout = ({ children, sub }: LayoutProps) => {
+export const Layout = ({children, sub, userName}: LayoutProps) => {
     const router = useRouter();
 
     const routeQuery = useMemo(() => {
         return qs.stringify(router.query);
-    }, [ router ]);
+    }, [router]);
 
     return <>
-        <AppBar position="sticky">
+        <AppBar position="fixed" sx={{zIndex: (theme) => theme.zIndex.drawer + 1}}>
             <Toolbar>
-                <Box sx={ { flexGrow: 1, display: { xs: 'none', md: 'flex' } } }>
-                    <Link href={ `/?${ routeQuery }` }>
-                        <Button sx={ { my: 2, color: 'white', display: 'block' } } component={ 'a' }>
+                <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}>
+                    <Link href={`/?${routeQuery}`}>
+                        <Button sx={{my: 2, color: 'white', display: 'block'}} component={'a'}>
                             Me
                         </Button>
                     </Link>
-                    <Link href={ `/team?${ routeQuery }` }>
-                        <Button sx={ { my: 2, color: 'white', display: 'block' } } component={ 'a' }>
+                    <Link href={`/team?${routeQuery}`}>
+                        <Button sx={{my: 2, color: 'white', display: 'block'}} component={'a'}>
                             Team
                         </Button>
                     </Link>
                 </Box>
-
+                {userName}
             </Toolbar>
         </AppBar>
-        <Box sx={ { display: 'flex' } }>
-            <Box sx={ { flexGrow: 1, } }>
-                { children }
+        <Box sx={{display: 'flex'}}>
+            <Box sx={{flexGrow: 1, paddingTop: 10}}>
+                {children}
             </Box>
             <Drawer
                 open
-                anchor={ 'right' }
-                variant={ 'permanent' }
-                PaperProps={ {
-                    sx: { width: drawerWidth }
-                } }
-                sx={ {
+                anchor={'right'}
+                variant={'permanent'}
+                PaperProps={{
+                    sx: {width: drawerWidth, paddingTop: 10}
+                }}
+                sx={{
                     width: drawerWidth,
                     flexShrink: 0,
-                } }>
-                <Settings sub={ sub }/>
+                }}>
+                <Settings sub={sub}/>
             </Drawer>
+        </Box>
+        <Box sx={{padding: 2}}>
+            {name} v{version}
         </Box>
     </>
 }
