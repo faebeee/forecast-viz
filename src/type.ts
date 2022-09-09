a51a6e0d0b1907a6a6006b55fe8816ca36d3889b
Fabio Gianini <faebeee@gmail.com>
2022-09-09 08:47:22 +0200

    Fixes

--- src/type.ts
+++ src/type.ts
@@ -1,2 +1,2 @@
 export type HoursPerDay = Record<string, { [key: string]: number }>
-export type HoursPerDayCollectionItem = { [key: string]: number, date: string };
+export type HoursPerDayCollectionItem = {  date: string, [key: string]: number | string };
