6f0675c8fea8e9781ecf38a107614825b0ac7b4b
Fabio Gianini <faebeee@gmail.com>
2022-10-13 18:11:16 +0200

    Improvements

--- src/server/services/get-time-entries-for-users.ts
+++ src/server/services/get-time-entries-for-users.ts
@@ -1,6 +1,5 @@
 import { getRedis } from "../redis";
 import { HarvestApi } from "../get-harvest";
-import { REDIS_CACHE_TTL } from "../../config";
 import { TimeEntry } from "../harvest-types";
 
 export const getTimeEntriesForUser = async (harvest: HarvestApi, userId: number, from: string, to: string): Promise<TimeEntry[]> => {
@@ -16,7 +15,7 @@ export const getTimeEntriesForUser = async (harvest: HarvestApi, userId: number,
 
     const results = await harvest.getTimeEntries({ userId, from, to });
 
-    if (redis) {
+    if (redis && results.length > 0) {
         await redis.set(redisKey, JSON.stringify(results));
         await redis.expire(redisKey, 60 * 15);
     }
@@ -36,13 +35,14 @@ export const getTimeEntriesForUsers = async (harvest: HarvestApi, ids: number[],
     }
 
     const results = await Promise.all(ids.map(id => getTimeEntriesForUser(harvest, id, from, to)))
+    const hasEmptyData = results.find((r) => r.length === 0);
     const allResults = results.reduce((acc, res) => {
         return acc.concat(res);
     }, []);
 
-    if (redis) {
+    if (redis && !hasEmptyData) {
         await redis.set(redisKey, JSON.stringify(allResults));
-        await redis.expire(redisKey, 60 * 15);
+        await redis.expire(redisKey, 60 * 30);
     }
 
     return allResults;
