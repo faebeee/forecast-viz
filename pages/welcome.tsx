58cc1fa56768cca70f2b0167c50635dff02baf1a
philipplaeubli <philipp.laeubli@dreipol.ch>
2022-11-19 13:32:29 +0100

    Fix auth flow

--- pages/welcome.tsx
+++ pages/welcome.tsx
@@ -5,53 +5,56 @@ import {ContentHeader} from "../src/components/content-header";
 import {useRouter} from "next/router";
 import {GetServerSideProps} from "next";
 import {withServerSideSession} from "../src/server/with-session";
+import {useState} from "react";
 
 
 export const getServerSideProps: GetServerSideProps = withServerSideSession(
     async ({query, req}): Promise<{ props: WelcomeProps }> => {
-        const harvestClientId = query.clientId as string ?? '';
+
+        const harvestClientId = query.clientId as string ?? undefined;
+        if (harvestClientId) {
+            req.session.clientId = harvestClientId
+            await req.session.save()
+        }
         return {
             props: {
-                harvestClientId,
+                harvestClientId: req.session.clientId ?? '',
             }
         }
     }
 )
 
 export type WelcomeProps = {
-    harvestClientId:string
+    harvestClientId?: string
 }
 
 
 export const Welcome = ({harvestClientId}: WelcomeProps) => {
     const router = useRouter();
+    const [harvestClient, setHarvestClient] = useState(harvestClientId)
 
     const onClick = () => {
-        router.push('https://id.getharvest.com/oauth2/authorize?client_id=' + harvestClientId + '&response_type=token');
+        router.push('https://id.getharvest.com/oauth2/authorize?client_id=' + harvestClient + '&response_type=token');
     }
 
     return <>
         <Layout hasAdminAccess={false} userName={''} active={'day'}>
             <Box sx={{flexGrow: 1,}}>
                 <Stack spacing={2}>
-                    <Typography variant={'h4'}>Settings</Typography>
+                    <ContentHeader title={'Welcome'} showPicker={false}/>
                     <TextField variant={'outlined'}
                                label={'Harvest Client Id'}
                                fullWidth
-                               value={harvestClientId}
-                    />
+                               value={harvestClient}
+                               onChange={(e) => setHarvestClient(e.target.value)}/>
 
-                </Stack>
-
-                <Box p={4}>
-                    <ContentHeader title={'Welcome'} showPicker={false}/>
                     <Button color={'primary'}
                             fullWidth
                             size={'large'}
                             variant={'contained'}
                             onClick={onClick}
                     >Authenticate</Button>
-                </Box>
+                </Stack>
             </Box>
         </Layout>
     </>
