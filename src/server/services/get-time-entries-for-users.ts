e0793e677bb7af91df57233cea4de0971ffbb8e0
philipplaeubli <philipp.laeubli@dreipol.ch>
2022-11-26 20:36:24 +0100

    simplified

--- src/server/services/get-time-entries-for-users.ts
+++ src/server/services/get-time-entries-for-users.ts
@@ -1,21 +1,16 @@
-import { HarvestApi } from "../get-harvest";
-import { TimeEntry } from "../harvest-types";
+import {HarvestApi} from "../get-harvest";
+import {TimeEntry} from "../harvest-types";
 import {getCache} from "./cache";
 import {DEFAULT_CACHE_TTL} from "../../config";
 
 export const getTimeEntriesForUser = async (harvest: HarvestApi, userId: number, from: string, to: string, projectId?: number): Promise<TimeEntry[]> => {
-    const redisKey = `services/entries/${ userId }-${ from }-${ to }-${projectId}`;
-    const cache = getCache();
-    const cachedResult = await cache.get(redisKey) as TimeEntry[]
-        if (!!cachedResult) {
-            return cachedResult;
-        }
-
-    const results = await harvest.getTimeEntries({ userId, from, to, project_id: projectId });
-
-    await cache.set(redisKey, results, DEFAULT_CACHE_TTL);
-
-    return results;
+    return await getCache().getAndSet(`services/entries/${userId}-${from}-${to}-${projectId}`, () =>
+        harvest.getTimeEntries({
+        userId,
+        from,
+        to,
+        project_id: projectId
+    }), DEFAULT_CACHE_TTL);
 }
 
 export const getTimeEntriesForUsers = async (harvest: HarvestApi, ids: number[], from: string, to: string, projectId?: number) => {
