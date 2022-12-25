8ccaca0c5b368dffa98b201aebd576978a655d94
philipplaeubli <philipp.laeubli@dreipol.ch>
2022-12-25 15:32:29 +0100

    Replace unneccessary promises with async/await

--- src/server/services/cache/local.ts
+++ src/server/services/cache/local.ts
@@ -1,39 +1,30 @@
-import { Cache, Key, Value, ValueCallback } from './types'
+import { Cache, Key, Value, ValueFunction } from './types'
 
 export const LocalInMemoryCache = (): Cache => {
   const data = new Map<Key, unknown>()
   return {
-    getAndSet<T extends Value>(
+    async getAndSet<T extends Value>(
       key: Key,
-      valueFunction: ValueCallback<T>,
+      valueFunction: ValueFunction<T>
     ): Promise<T> {
-      return new Promise<T>((resolve) => {
-        this.get(key).then((value) => {
-          if (value) {
-            resolve(value as T)
-          } else {
-            valueFunction().then((newValue) => {
-              resolve(newValue)
-              this.set(key, newValue)
-            })
-          }
-        })
-      })
+      const value = await this.get(key)
+      if (value) {
+        return value as T
+      } else {
+        const newValue = await valueFunction()
+        this.set(key, newValue)
+        return newValue
+      }
     },
-    flush(): Promise<void> {
-      return new Promise<void>((resolve) => {
-        data.clear()
-        resolve()
-      })
+    async flush(): Promise<void> {
+      return data.clear()
     },
-    get<T extends Value>(key: Key): Promise<T> {
-      return new Promise<T>((resolve) => {
-        if (data.has(key)) {
-          resolve(data.get(key) as T)
-        } else {
-          resolve(null as T)
-        }
-      })
+    async get<T extends Value>(key: Key): Promise<T> {
+      if (data.has(key)) {
+        return data.get(key) as T
+      } else {
+        return null as T
+      }
     },
     set<T>(key: string, value: T): void {
       data.set(key, value)
--- src/server/services/cache/redis.ts
+++ src/server/services/cache/redis.ts
@@ -1,89 +1,63 @@
 import Redis from 'ioredis'
 
-import { Cache, Key, Value, ValueCallback } from './types'
-import {DEFAULT_CACHE_TTL} from "../../../config";
+import { Cache, Key, Value, ValueFunction } from './types'
+import { DEFAULT_CACHE_TTL } from '../../../config'
 
 export const RedisCache = (): Cache => {
   const redis = new Redis(process.env.REDIS_URL as string)
-    redis.on('connect', () => {
-        console.log('CacheStore - Connection status: connected');
-    });
-    redis.on('end', () => {
-        console.log('CacheStore - Connection status: disconnected');
-    });
-    redis.on('reconnecting', () => {
-        console.log('CacheStore - Connection status: reconnecting');
-    });
-    redis.on('error', (err) => {
-        console.log('CacheStore - Connection status: error ', { err });
-    });
+  redis.on('connect', () => {
+    console.log('CacheStore - Connection status: connected')
+  })
+  redis.on('end', () => {
+    console.log('CacheStore - Connection status: disconnected')
+  })
+  redis.on('reconnecting', () => {
+    console.log('CacheStore - Connection status: reconnecting')
+  })
+  redis.on('error', (err) => {
+    console.log('CacheStore - Connection status: error ', { err })
+  })
   return {
-    getAndSet<T extends Value>(
+    async getAndSet<T extends Value>(
       key: Key,
-      valueFunction: ValueCallback<T>,
-      ttl: number = DEFAULT_CACHE_TTL,
+      valueFunction: ValueFunction<T>,
+      ttl: number = DEFAULT_CACHE_TTL
     ): Promise<T> {
-      return new Promise<T>((resolve) => {
-        this.get(key).then((value) => {
-          if (value) {
-            resolve(value as T)
-          } else {
-            valueFunction().then((newValue) => {
-              resolve(newValue)
-              this.set(key, newValue, ttl)
-            })
-          }
-        })
-      })
+      const value = await this.get(key)
+      if (value) {
+        return value as T
+      } else {
+        const newValue = await valueFunction()
+        this.set(key, newValue, ttl)
+        return newValue
+      }
     },
-    flush(): Promise<void> {
-      return new Promise<void>((resolve, reject) => {
-        redis.flushdb((err) => {
-          if (err) {
-            reject()
-          } else {
-            resolve()
-          }
-        })
-      })
+    async flush(): Promise<void> {
+      await redis.flushdb()
     },
-    get<T extends Value>(key: Key): Promise<T> {
-      let finalResult : any = null
-      return new Promise((resolve, reject) => {
-        try {
-          redis.hgetall(key, (err, result) => {
-
-            // guard: if there is an error, reject the promise and bail out.
-            if (err) {
-              reject()
-              return;
-            }
-
-            // guard: if there is no result, resolve the promise with null and bail out.
-            if (!(result && result.type)) {
-              resolve(finalResult)
-              return;
-            }
-
-            // we got something and need to convert the value back to the proper type.
-            if (result.type === 'number') {
-              finalResult = Number(result.data) as T // we need to convert the stringified number back to a number
-            } else if (result.type === 'string') {
-              finalResult = result.data as T // no conversion needed, ioredis stores all them values as strings
-            } else {
-              finalResult = JSON.parse(result.data) as T // convert the json string back to an object
-            }
-          })
-        } catch (e) {
-          // upon any error, log it and remove the cache key so that we don't get the same error twice.
-          // This also might mitigate the issue if the error is caused by a bad value in the cache
-          console.log(`unable to retrieve/parse value of cache key:${key}`)
-          redis.del(key)
-        } finally {
-          // what ever happens - resolve the promise
-          resolve(finalResult)
+    async get<T extends Value>(key: Key): Promise<T> {
+      let finalResult: any = null
+      try {
+        finalResult = await redis.hgetall(key)
+        // guard: if there is no result, resolve the promise with null and bail out.
+        if (!(finalResult && finalResult.type)) {
+          return null as T
         }
-      })
+        // we got something and need to convert the value back to the proper type.
+        if (finalResult.type === 'number') {
+          finalResult = Number(finalResult.data) as T // we need to convert the stringified number back to a number
+        } else if (finalResult.type === 'string') {
+          finalResult = finalResult.data as T // no conversion needed, ioredis stores all them values as strings
+        } else {
+          finalResult = JSON.parse(finalResult.data) as T // convert the json string back to an object
+        }
+      } catch (e) {
+        // upon any error, log it and remove the cache key so that we don't get the same error twice.
+        // This also might mitigate the issue if the error is caused by a bad value in the cache
+        console.log(`unable to retrieve/parse value of cache key:${key}`)
+        redis.del(key)
+      }
+      return finalResult
     },
     set<T extends Value>(key: string, value: T, ttl = DEFAULT_CACHE_TTL): void {
       if (value === null) {
